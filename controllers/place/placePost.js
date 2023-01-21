ffec2365f7248e7b4dc37c30cc2fa071330c4e05
Corey Hall <cohall15@gmail.com>
2023-01-21 11:29:41 -0500

    modify index and post

--- controllers/place/placePost.js
+++ controllers/place/placePost.js
@@ -6,20 +6,18 @@
 // for MongoDB database
 const Place = require("../../models/place")
 
-// const placePost = async (req, res) => {
-//     try {
-//         //  create place
-//         res.json (await Place.create(req.body));
-//     } catch (error) {
-//         //send error
-//         res.status(400).json(error)
-//     }
-// };
-
-const placePost = (req, res) => {
-    
-    res.send("post place controller (create)")
+/ {
+        //  create place
+    const placePost = async (req, res) => {
+    try res.json (await Place.create(req.body));
+    } catch (error) {
+        //send error
+        res.status(400).json(error)
+    }
 };
+
+
+
 
 //----------------------------
 //  Export controller function
