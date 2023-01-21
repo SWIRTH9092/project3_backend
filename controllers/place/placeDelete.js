28527765664fe4a7719dba124f27a23d7e43cdfd
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-01-21 11:14:49 -0500

    updated delete show and update routes

--- controllers/place/placeDelete.js
+++ controllers/place/placeDelete.js
@@ -1,24 +1,23 @@
 //----------------------------
 //  Import dependencies
 //----------------------------
-// const express = require("express")
 
 // for MongoDB database
 const Place = require("../../models/place")
 
-// const placeDelete = async (req, res) => {
-//     try {
-//         // send place to delete
-//         res.json(await Place.findByIdAndRemove(req.params.id))
-//     } catch (error) {
-//         //send error
-//         res.status(400).json(error);
-//     }
-// };
-
-const placeDelete = (req, res) => {
-    res.send("Delete place controller (delete)")
+const placeDelete = async (req, res) => {
+    try {
+        // send place to delete
+        res.json(await Place.findByIdAndRemove(req.params.id))
+    } catch (error) {
+        //send error
+        res.status(400).json(error);
+    }
 };
+
+// const placeDelete = (req, res) => {
+//     res.send("Delete place controller (delete)")
+// };
 
 //----------------------------
 //  Export controller function
--- controllers/place/placeShow.js
+++ controllers/place/placeShow.js
@@ -1,24 +1,23 @@
 //----------------------------
 //  Import dependencies
 //----------------------------
-// const express = require("express")
 
 // for MongoDB database
 const Place = require("../../models/place")
 
-// const placeShow = async (req, res) => {
-//     try {
-//         res.json(await Place.findById(req.params.id));
-//     } catch (error) {
-//         //send error
-//         res.status(400).json(error);
-//     }
-// };
-
-const placeShow = (req, res) => {
-   
-    res.send("Show place controller (get one item)")
+const placeShow = async (req, res) => {
+    try {
+        res.json(await Place.findById(req.params.id));
+    } catch (error) {
+        //send error
+        res.status(400).json(error);
+    }
 };
+
+// const placeShow = (req, res) => {
+   
+//     res.send("Show place controller (get one item)")
+// };
 
 
 //----------------------------
